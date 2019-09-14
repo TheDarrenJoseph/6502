4954abb7650e7487af8d214e06ab69b34ab37684
Darren Joseph <thedarrenjoseph@gmail.com>
2019-09-14 20:27:47 +0100

    Update comments

--- C64/HelloWorld.asm
+++ C64/HelloWorld.asm
@@ -1,14 +1,14 @@
     processor 6502
-    org $1000
+    org $1000 # load into address 4096
 ;https://stackoverflow.com/questions/43498617/ascii-to-c64-screen-codes-in-dasm-assembler
 ;Original by nexus_2006
-    ldx #$00    ;using x register as column counter
+    ldx #$00    ;x=0
 printloop:
-    lda hexmsg,x;load a with x bit from message
+    lda hexmsg,x;load bit x from hexmsg byte
     sta $0400,x ;store this bit in row 0 col 0 address
     inx         ;x++
-    cpx #$0B    ;is x >= 6?
-    bne printloop   ;if not x >= 5, loop again
-    rts         ;return from program
-
+    cpx #$0B    ;Compare X to Hex 11
+    bne printloop   ;if (not equal) loop again
+    rts         ;else return from program
+;define a series of Hex bytes for our message
 hexmsg .byte $08,$05,$0c,$0c,$0f,$20,$17,$0F,$12,$0C,$04
